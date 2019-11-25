5dc828a4aae2927f3c7170af7e5ab4b1a106e478
[cunhamuril] <[murilo.sant@hotmail.com]>
2019-11-25 14:40:45 -0300

    fix bug delete user

--- delete-user.php
+++ delete-user.php
@@ -5,12 +5,18 @@
   
   $con = mysqli_connect("localhost", "root", "12341234", "projeto_lbd") or die("Sem conexão com o servidor de banco de dados");
   
-  $query = "DELETE FROM user WHERE id = $id";
-  mysqli_query($con, $query);
+  $query[0] = "DELETE FROM movies WHERE id_user = $id";  
+  $query[1] = "DELETE FROM series WHERE id_user = $id";  
+  $query[2] = "DELETE FROM movie_genres WHERE id_user = $id";
+  $query[3] = "DELETE FROM serie_genres WHERE id_user = $id";
+  $query[4] = "DELETE FROM user WHERE id = $id";
+
+  foreach($query as $i) {
+    mysqli_query($con, $i);
+  }
 
   unset($_SESSION['login']);
   unset($_SESSION['password']);  
   header('location:signin.php');
 
   session_destroy();
-?>
